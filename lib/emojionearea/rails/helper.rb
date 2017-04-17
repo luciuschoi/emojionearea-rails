26d33cb6b8951840392d9a48e695ba8d7e458d9f
Lucius Choi <lucius.choi@gmail.com>
2017-04-17 19:52:14 +0900

    add some helpers

--- lib/emojionearea/rails/helper.rb
+++ lib/emojionearea/rails/helper.rb
@@ -8,11 +8,34 @@ module Emojionearea
         text_symbol = Rumoji.encode(content)
         text_symbol.gsub(/:([\w+-]+):/) do |match|
           if emoji = Emoji.find_by_alias($1)
+            %(<img alt="#{emoji.raw}" src="assets/png_64/#{emoji.image_filename.split("/").last}" style="vertical-align:middle" width="16" height="16" />)
+          else
+            match
+          end
+        end.html_safe if content.present?
+      end
+      def gemojify_cdn(content)
+        text_symbol = Rumoji.encode(content)
+        text_symbol.gsub(/:([\w+-]+):/) do |match|
+          if emoji = Emoji.find_by_alias($1)
             %(<img alt="#{emoji.raw}" src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.1.4/assets/png/#{emoji.image_filename.split("/").last}" style="vertical-align:middle" width="16" height="16" />)
           else
             match
           end
         end.html_safe if content.present?
+      end
+      def gemojify_wp(content)
+        text_symbol = Rumoji.encode(content)
+        text_symbol.gsub(/:([\w+-]+):/) do |match|
+          if emoji = Emoji.find_by_alias($1)
+            %(<img alt="#{emoji.raw}" src="http://emojione.com/wp-content/uploads/assets/emojis/#{emoji.image_filename.split("/").last.split(".").first}.svg" style="vertical-align:middle" width="16" height="16" />)
+          else
+            match
+          end
+        end.html_safe if content.present?
+      end
+      def emojify_code(content)
+        text_symbol = Rumoji.encode(content) {|emoji| emoji.code } if content.present?
       end
     end
   end
